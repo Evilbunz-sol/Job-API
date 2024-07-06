c4ddff947a1213f6a652c8bed9f1b58332ebb5e1
Evilbunz <jameel.altamash@gmail.com>
2024-07-06 19:53:44 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+// Core Packages
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
+const app = express();
+
+// Security Packages
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const rateLimiter = require("express-rate-limit")
+
+// Connect DB
+const connectDB = require("./db/connect")
+
+// Middleware
+const authenticateUser = require("./middleware/authentication")
+
+// Routers
+const authRouter = require("./routes/auth")
+const jobRouter = require("./routes/jobs")
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// Application Start
+app.use(express.json())
+
+app.set("trust proxy", 1)
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 Minutes
+  max: 100  // Limit each IP to 100 requests per windowMs (15minutes)
+}))
+
+// routes
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/jobs", authenticateUser, jobRouter)
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
